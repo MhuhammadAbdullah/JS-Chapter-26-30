2f2664e5dc3436f8a1cbed7d2625eed2d2a0edd0
Muhammad Abdullah <abaloch0033@gmail.com>
2024-10-07 15:49:15 +0500

    updated

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 // /QUESRTIOPN 1
-// var a = +prompt("Enter a positive number")
-// console.log("Number: " + a);
-// console.log("Round off value: " + a.round());
-// console.log("floor value of the number" + a.float());
-// console.log("ceil value of the number: " + a.ceil());
+// var number = prompt("Enter a positive number");
+// var num = parseFloat(number);
+// alert("Number: " + number);
+// alert("Round off value: " + Math.round(number));
+// alert("Floor value: " + Math.floor(number));
+// alert("Ceil value: " + Math.ceil(number));
 
 
 // QUESTION 2
-// var a = +prompt("Enter a negative number")
-// console.log("Number: " + a);
-// console.log("Round off value: " + a.round());
-// console.log("floor value of the number" + a.float());
-// console.log("ceil value of the number: " + a.ceil());
+// var number = prompt("Enter a negative number");
+// var num = parseFloat(number);
+// alert("Number: " + number);
+// alert("Round off value: " + Math.round(number));
+// alert("Floor value: " + Math.floor(number));
+// alert("Ceil value: " + Math.ceil(number));
 
 
 
@@ -21,13 +23,13 @@
 
 
 // QUESTION 4
-// var ran=Math.random();
+// var ran=Math.floor(Math.random()*6)+1;
 // console.log("Value of dice is: "+ran);
 
 
 // QUESTION 5
-// var ran=Math.random()*2 + 1;
-// if(ran==3){
+// var ran=Math.floor(Math.random()*2) + 1;
+// if(ran===1){
 //     console.log("Random coin value: Heads");
 // }
 // else{
@@ -36,18 +38,19 @@
 
 
 // QUESTION 6
-// var ran=Math.random()*100 +1;
+// var ran=Math.round(Math.random()*100 )+1;
 // console.log("random Number between 1 and 100 is: "+ran);
 
 
 // QUESTION 7
-// var p=+prompt("weight")
-// console.log("The weight of user is "+p+" kilograms");
+// var userInput = prompt("Enter your weight (e.g., 50, 50kgs, 50.2kgs, 50.2kilograms):");
+// var weight = parseFloat(userInput);
+// console.log("The weight of user is "+ weight +" kilograms");
 
 
 // QUESTION 8
 // var n = Math.random() * 10 + 1;
-// var promp = +prompt("o input a number between 1 and 10")
+// var promp = prompt("Enter a number between 1 and 10")
 // if (n == promp) {
 //     console.log("Congratulations");
 // }
